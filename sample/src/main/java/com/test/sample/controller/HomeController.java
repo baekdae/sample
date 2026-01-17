568fba02e43537c4928febe052720c94b94d55fe
baekdae <baekdae@gmail.com>
2026-01-17 14:19:54 +0900

    init

--- sample/src/main/java/com/test/sample/controller/HomeController.java
+++ sample/src/main/java/com/test/sample/controller/HomeController.java
@@ -10,7 +10,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-
+	/**
+	 * test
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 */
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
